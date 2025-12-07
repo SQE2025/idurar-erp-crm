963e44649f66e0ba486090404e9ef80db59b8f55
im-sami <i222717@nu.edu.pk>
2025-12-07 18:49:45 +0500

    Fix backend api not being accessable in staging and production

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,7 +1,6 @@
-export const API_BASE_URL =
-  import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
-    ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
-    : 'http://localhost:8888/api/';
+import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
+  ? '/api/'
+  : 'http://localhost:8888/api/';
 export const BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER
